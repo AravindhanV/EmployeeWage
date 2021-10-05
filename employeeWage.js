0c52c27a06813b40d7f51914665fac2153fb73d8
Aravindhan V <aravindhan.v@perfios.com>
2021-10-05 19:06:44 +0530

    ADDED: logic to calculate working hours of an employee

--- employeeWage.js
+++ employeeWage.js
@@ -10,4 +10,24 @@
     }
 }
 
+//UC2
+{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
 
+    let employeeHrs = 0;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    switch (empCheck) {
+        case IS_PART_TIME:
+            employeeHrs = PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            employeeHrs = FULL_TIME_HOURS;
+            break;
+        default:
+            employeeHrs = 0;
+    }
+}
