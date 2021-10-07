e39266e676b0fae221af4a969ff82f40aeaa96e5
Aravindhan V <varavindhan2010@gmail.com>
2021-10-07 17:30:15 +0530

    ADDED: storage of day, empHrs and empWage together using Objects

--- employeeWage.js
+++ employeeWage.js
@@ -162,3 +162,36 @@ empDailyHrsMap.forEach((value, key) => {
 console.log("Full Working Days: " + fullWorkingDays);
 console.log("Half Working Days: " + halfWorkingDays);
 console.log("No Working Days: " + noWorkingDays);
+
+//UC10
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyHrsAndWageArray = new Array();
+while (
+  totalEmpHrs <= MAX_HRS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  let empHrs = getWorkingHours(empCheck);
+  totalEmpHrs += empHrs;
+  empDailyHrsAndWageArray.push({
+    dayNum: totalWorkingDays,
+    dailyHours: empHrs,
+    dailyWage: calcDailyWage(empHrs),
+    toString() {
+      return (
+        "\nDay" +
+        this.dayNum +
+        "=> Working Hours is " +
+        this.dailyHours +
+        "And Wage Earned = " +
+        this.dailyWage
+      );
+    },
+  });
+}
+
+console.log(
+  "UC 10 Showing Daily worked and wage earned: " + empDailyHrsAndWageArray
+);
